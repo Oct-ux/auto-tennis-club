292e6daa075ab8d7f567a350acb2373012211c1f
Oct-ux <332016758+Oct-ux@users.noreply.github.com>
2026-09-21 20:30:04 +0200

    feat: build Android MVP BLE and machine foundations

--- app/src/main/java/com/autotennisclub/app/pusun/PusunNotification.kt
+++ app/src/main/java/com/autotennisclub/app/pusun/PusunNotification.kt
@@ -5,5 +5,4 @@ sealed interface PusunNotification {
     data class Fault(val code: Int, val type: FaultType) : PusunNotification
     data class Unknown(val command: Int, val data: ByteArray) : PusunNotification
 }
-
 enum class FaultType { WHEEL_PROTECTION, ENTRANCE_PROTECTION, NO_BALLS, UNKNOWN }
